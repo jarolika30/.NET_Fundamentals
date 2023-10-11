562f37120a9e5e621715fd74f4defe4ee0938a3c
Katsiaryna Staurova <Katsiaryna_Staurova@epam.com>
2023-10-11 14:59:16 +0300

    create first .Net core console app

---
+++ DotNetCoreConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please input your name!");
+var name = Console.ReadLine();
+string output = $"Hello, {name}!";
+Console.WriteLine(output);
+
