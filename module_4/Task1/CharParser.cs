0015bb3ecb75fb8b35f4b10c838a1c877cbbeade
Katsiaryna Staurova <Katsiaryna_Staurova@epam.com>
2023-11-09 20:14:22 +0300

    implement task 1

---
+++ module_4/Task1/CharParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    internal class CharParser
+    {
+        public static void printFirstChar(string line)
+        {
+            try
+            {
+                Console.WriteLine($"First char of your input was '{line[0]}'");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The string is null or empty");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
--- module_4/Task1/Program.cs
+++ module_4/Task1/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Task1
+﻿using System;
+
+namespace Task1
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
             // TODO: Implement the task here.
+            string input = string.Empty;
+
+            while (true)
+            { 
+                Console.WriteLine("Please enter some line or input 'exit' to exit");
+                input = Console.ReadLine();
+
+                if ("exit".Equals(input, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    break;
+                }
+
+                CharParser.printFirstChar(null);
+            }
         }
     }
 }
