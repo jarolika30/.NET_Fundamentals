c57a35e4ec7cc590e02b4def208100a81c2a7628
Katsiaryna Staurova <Katsiaryna_Staurova@epam.com>
2023-11-15 18:30:57 +0300

    return all found files and folders in the form

--- module_3/FileWatcherApp/FileWatcher.cs
+++ module_3/FileWatcherApp/FileWatcher.cs
@@ -7,56 +7,67 @@ using System.IO;
 
 namespace FileWatcherApp
 {
-    internal class FileWatcher
+    public class FileWatcher
     {
         private FileSystemWatcher fileSystemWatcher1;
+        public delegate void FileMonitoring(string message);
+        public event FileMonitoring LogWork;
 
         internal FileWatcher ()
         {
-            this.fileSystemWatcher1 = new FileSystemWatcher();
+            fileSystemWatcher1 = new FileSystemWatcher();
         }
 
-        public void startfileSystemWatcher()
+        public void StartfileSystemWatcher()
         {
-            this.fileSystemWatcher1.EnableRaisingEvents = true;
-            this.fileSystemWatcher1.IncludeSubdirectories = true;
-            this.fileSystemWatcher1.Changed += new FileSystemEventHandler(this.fileWasChanged);
-            this.fileSystemWatcher1.Created += new FileSystemEventHandler(this.fileWasCreated);
-            this.fileSystemWatcher1.Deleted += new FileSystemEventHandler(this.fileWasDeleted);
-            this.fileSystemWatcher1.Error += new ErrorEventHandler(this.fileSystemError);
-            this.fileSystemWatcher1.Renamed += new RenamedEventHandler(this.fileWasRenamed);
+            fileSystemWatcher1.EnableRaisingEvents = true;
+            fileSystemWatcher1.IncludeSubdirectories = true;
+            fileSystemWatcher1.Changed += new FileSystemEventHandler(FileWasChanged);
+            fileSystemWatcher1.Created += new FileSystemEventHandler(FileWasCreated);
+            fileSystemWatcher1.Deleted += new FileSystemEventHandler(FileWasDeleted);
+            fileSystemWatcher1.Error += new ErrorEventHandler(FileSystemError);
+            fileSystemWatcher1.Renamed += new RenamedEventHandler(FileWasRenamed);
         }
 
-        public void setFolderNameToWatch(string name)
+        public void SetFolderNameToWatch(string name)
         {
-            // C:\\Users\\Katsiaryna_Staurova\\Documents
-            string folderName = name;
-            this.fileSystemWatcher1.Path = folderName;
+            string folder = name;
+            fileSystemWatcher1.Path = folder;
         }
 
-        private void fileWasChanged(object sender, FileSystemEventArgs e)
+        public void StopFileWatcher()
         {
-            MessageBox.Show($"File {e.Name} was changed. \n Full path: {e.FullPath}");
+            fileSystemWatcher1.EnableRaisingEvents = false;
+            fileSystemWatcher1.Changed -= new FileSystemEventHandler(FileWasChanged);
+            fileSystemWatcher1.Created -= new FileSystemEventHandler(FileWasCreated);
+            fileSystemWatcher1.Deleted -= new FileSystemEventHandler(FileWasDeleted);
+            fileSystemWatcher1.Error -= new ErrorEventHandler(FileSystemError);
+            fileSystemWatcher1.Renamed -= new RenamedEventHandler(FileWasRenamed);
         }
 
-        private void fileWasCreated(object sender, FileSystemEventArgs e)
+        private void FileWasChanged(object sender, FileSystemEventArgs e)
         {
-            MessageBox.Show($"File {e.Name} was created.\nFull path: {e.FullPath}");
+            LogWork.Invoke($"File {e.Name} was changed.\n Full path: {e.FullPath}");
         }
 
-        private void fileWasDeleted(object sender, FileSystemEventArgs e)
+        private void FileWasCreated(object sender, FileSystemEventArgs e)
         {
-            MessageBox.Show($"File {e.Name} was deleted.\nFull path: {e.FullPath}");
+            LogWork.Invoke($"File {e.Name} was created.\nFull path: {e.FullPath}");
         }
 
-        private void fileSystemError(object sender, ErrorEventArgs e)
+        private void FileWasDeleted(object sender, FileSystemEventArgs e)
         {
-            MessageBox.Show($"Sorry, an error occurs during the file processing.\nError: {e.GetException()}");
+            LogWork.Invoke($"File {e.Name} was deleted.\nFull path: {e.FullPath}");
         }
 
-        private void fileWasRenamed(object sender, RenamedEventArgs e)
+        private void FileSystemError(object sender, ErrorEventArgs e)
         {
-            MessageBox.Show($"File {e.Name} was renamed.\nFull path: {e.FullPath}");
+            LogWork.Invoke($"Sorry, an error occurs during the file processing.\nError: {e.GetException()}");
+        }
+
+        private void FileWasRenamed(object sender, RenamedEventArgs e)
+        {
+            LogWork.Invoke($"File {e.Name} was renamed.\nFull path: {e.FullPath}");
         }
     }
 }
---
+++ module_3/FileWatcherApp/Form2.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileWatcherApp
+{
+    public partial class Form2 : Form
+    {
+        private FileWatcher watcher;
+
+        public Form2()
+        {
+            watcher = new FileWatcher();
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string rootPath = textBox1.Text;
+            var files = from file in Directory.EnumerateFiles(rootPath) select file;
+            var fileString = $"List of Files: {Environment.NewLine}";
+
+            int amount = 0;
+            richTextBox1.Text = fileString;
+
+            foreach (var file in files)
+            {
+                richTextBox1.Text += $"{file}{Environment.NewLine}";
+                amount++;
+            }
+
+            richTextBox1.AppendText($"Total files - {amount}{Environment.NewLine}");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string folderName = textBox1.Text;
+            richTextBox1.Text += $"You have started your FileWatcher...{Environment.NewLine}";
+            
+            if (!string.IsNullOrEmpty(folderName))
+            {
+                watcher.SetFolderNameToWatch(folderName);
+                watcher.StartfileSystemWatcher();
+                watcher.LogWork += fileChangeHandler;
+            } else
+            {
+                richTextBox1.Text += $"Please select the correct folder name...{Environment.NewLine}";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += $"You have stopped your FileWatcher... {Environment.NewLine}";
+            watcher.StopFileWatcher();
+        }
+
+        private void fileChangeHandler(string text)
+        {
+            richTextBox1.Text += text;
+        }
+    }
+}
